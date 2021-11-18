09356c500c4409597f27678138707a44a997d36b
AbhigyaShridhar <55044948+AbhigyaShridhar@users.noreply.github.com>
2021-11-18 19:05:09 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,13 +3,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from '@firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-_WHPad8FlsDcLURpGusEXVZ2F2AnkXs",
-  authDomain: "pomodoro-84ef2.firebaseapp.com",
-  projectId: "pomodoro-84ef2",
-  storageBucket: "pomodoro-84ef2.appspot.com",
-  messagingSenderId: "880371991959",
-  appId: "1:880371991959:web:2ecc01a6cfc0570f10f0d2",
-  measurementId: "G-P6DPFPS29B"
+  <config data>
 };
 
 const app = initializeApp(firebaseConfig);
